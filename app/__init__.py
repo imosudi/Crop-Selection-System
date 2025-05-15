87f14b3514317f82a2c911c030ee1ba4a29f1e9b
Mosudi I. O. <imosudi@gmail.com>
2025-05-15 18:32:35 +0100

    2nd Iterations

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask,redirect,url_for,render_template,request
-
+from flask_moment import Moment
 
 app=Flask(__name__)
 
+moment=Moment(app)
 
 from .routes import *
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
-from flask import Flask,redirect,url_for,render_template,request
-
+from flask import Flask, redirect, url_for, render_template, request
 from app import app
+from datetime import datetime
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method=='POST':
-        # Handle POST Request here
-        return render_template('index.html')
-    return render_template('index.html')
+    if request.method == 'POST':
+        # Future form processing can be added here
+        return render_template('index.html', current_year=datetime.now().year)
+    return render_template('index.html', current_year=datetime.now().year)
+
+@app.route('/how-it-works')
+def how_it_works():
+    return render_template('how_it_works.html', current_year=datetime.now().year)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', current_year=datetime.now().year)
